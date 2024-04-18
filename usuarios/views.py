6e6da8785492171c60b42f2a8b65ea5b0521bc36
Jose-Humberto-07 <jhpnascimento96@gmail.com>
2024-04-18 10:58:01 -0300

    created app medico

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib import auth
 
 # Create your views here.
 
@@ -48,4 +49,23 @@ def cadastro(request):
 
 def login_view(request):
     if request.method == "GET":
+        print(request.user)
         return render(request, 'login.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get("senha")
+
+        user = auth.authenticate(request, username=username, password=senha)
+
+        if user:
+            auth.login(request, user)
+            return redirect('/pacientes/home')
+        
+        messages.add_message(request, constants.ERROR, 'Usuário ou senha incorretos')
+        return redirect('/usuarios/login')
+
+    
+
+def sair(request):
+    auth.logout(request)
+    return redirect('/usuarios/login')
